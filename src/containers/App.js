13be4598b4b9393e4a47d5dbcdf02d213417638a
EvanBrightside <xvanx84@gmail.com>
2018-08-11 23:02:50 +0300

    set page actions

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
 import { hot } from 'react-hot-loader'
+import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+import User from '../components/User'
+import Page from '../components/Page'
+import * as pageActions from '../actions/PageActions'
 
 class App extends Component {
   render() {
-    const { name } = this.props.user
-    const { year, photos } = this.props.page
+    const { user, page } = this.props
+    const { setYear } = this.props.pageActions
     return <div>
-      <p>Привет из {name}!</p>
-      <p>У тебя {photos.length} фото за {year} год</p>
+      <User name={user.name} />
+      <Page photos={page.photos} year={page.year} setYear={setYear} />
     </div>
   }
 }
@@ -20,4 +24,10 @@ function mapStateToProps (state) {
   }
 }
 
-export default hot(module)(connect(mapStateToProps)(App))
+function mapDispatchToProps(dispatch) {
+  return {
+    pageActions: bindActionCreators(pageActions, dispatch)
+  }
+}
+
+export default hot(module)(connect(mapStateToProps, mapDispatchToProps)(App))
